c5f0227db66b45ea265e362c70772c487f4e6a9e
Madeline OLeary <madeline.w.oleary@gmail.com>
2023-06-23 15:34:16 +0200

    determines asset path based on env

--- h/assets.py
+++ h/assets.py
@@ -1,16 +1,22 @@
 """View for serving static assets under `/assets`."""
 
+import os
 import importlib_resources
 from h_assets import Environment, assets_view
 from pyramid.settings import asbool
+
 
 
 def includeme(config):
     auto_reload = asbool(config.registry.settings.get("h.reload_assets", False))
     h_files = importlib_resources.files("h")
 
+    asset_path = "/hypothesis/assets"
+    if "ASSET_PATH" in os.environ:
+        asset_path = os.environ["ASSET_PATH"]
+
     assets_env = Environment(
-        assets_base_url="/hypothesis/assets",
+        assets_base_url=asset_path,
         bundle_config_path=h_files / "assets.ini",
         manifest_path=h_files / "../build/manifest.json",
         auto_reload=auto_reload,
